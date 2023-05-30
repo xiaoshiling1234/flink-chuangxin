7fd54ce85d0d71751a3eca70e45beb307c7ffcd8
xiaoshiling1234 <564016339@qq.com>
2023-05-30 23:38:49 +0800

    备份

---
+++ flink/src/main/java/com/chuangxin/common/GlobalConfig.java
@@ -0,0 +1,8 @@
+package com.chuangxin.common;
+
+public class GlobalConfig {
+    public static final String REDIS_HOST="www.fastsay.cn";
+    public static final Integer REDIS_PORT=6379;
+    public static final String REDIS_PASSWORD="123456";
+    public static final Integer REDIS_DATABASE=0;
+}
