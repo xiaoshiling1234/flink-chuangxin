f5337824acc4a6b9688ac35bcc85bd738cf1de3b
xiaoshiling1234 <564016339@qq.com>
2023-06-01 23:46:31 +0800

    代码修改

--- flink/src/main/java/com/chuangxin/bean/api/BaseApiPO.java
+++ flink/src/main/java/com/chuangxin/bean/api/BaseApiPO.java
@@ -1,10 +1,32 @@
 package com.chuangxin.bean.api;
 
-import lombok.Data;
 
-@Data
 public class BaseApiPO {
     String client_id="6caa041c0a0a01092042d2f59e8c7118";
     String access_token="03635666-f281-4613-992e-b815b4689d03";
     String scope="read_cn";
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
 }
--- flink/src/main/java/com/chuangxin/bean/api/PatentSearchExpressionPO.java
+++ flink/src/main/java/com/chuangxin/bean/api/PatentSearchExpressionPO.java
@@ -1,12 +1,49 @@
 package com.chuangxin.bean.api;
 
-import lombok.Data;
-
-@Data
 public class PatentSearchExpressionPO extends BaseApiPO{
     String express="((名称+摘要和说明+权利要求书+说明书全文=无人机) OR (关键词=无人机) OR (技术领域=无人机) OR (背景技术=无人机) OR (发明内容=无人机) OR (具体实施方式=无人机) OR (附图说明=无人机))";
     String page="1";
     String sort_column="+PD";
     String exactSearch="1";
     String page_row="50";
+
+    public String getExpress() {
+        return express;
+    }
+
+    public void setExpress(String express) {
+        this.express = express;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public String getSort_column() {
+        return sort_column;
+    }
+
+    public void setSort_column(String sort_column) {
+        this.sort_column = sort_column;
+    }
+
+    public String getExactSearch() {
+        return exactSearch;
+    }
+
+    public void setExactSearch(String exactSearch) {
+        this.exactSearch = exactSearch;
+    }
+
+    public String getPage_row() {
+        return page_row;
+    }
+
+    public void setPage_row(String page_row) {
+        this.page_row = page_row;
+    }
 }
