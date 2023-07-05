d0f0e5a45c71a0b0c314f4c8ad2158de998b848c
564016339@qq.com <564016339@qq.com>
2023-07-05 13:51:30 +0800

    修改表达式和增量条件

--- flink/src/main/java/com/chuangxin/bean/api/BasePageExpressPO.java
+++ flink/src/main/java/com/chuangxin/bean/api/BasePageExpressPO.java
@@ -1,7 +1,16 @@
 package com.chuangxin.bean.api;
 
 public class BasePageExpressPO extends BaseApiPO{
-    String express="(名称=无人机)";
+    String express="(\n" +
+            " (名称,摘要和说明,权利要求书,说明书全文 +=  \n" +
+            " ( '无人驾驶飞机' or  '无人机' or 'unmanned aerial vehicle' or 'drone' or  '无人飞机'  OR 'UAV' or  'unmanned drone'  or '不载人飞机' or '无人驾驶飞行器'or '无人飞行器'or '无人直升机' or '无人直升飞机'  or '无人飞行系统'\n" +
+            ") \n" +
+            " )\n" +
+            "or \n" +
+            "(名称,摘要和说明,权利要求书,说明书全文 += ('无人' or '不载人') and ('飞机' or '飞行系统' or '飞行器' or '直升机' or '直升飞机' or '航空器'or '航空装备') ) \n" +
+            "or   \n" +
+            "(分类号 =  ( B64U% ) )\n" +
+            ") ";
     String page="1";
     String page_row="50";
     String sort_column;
